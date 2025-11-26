21d505b3fabcad3a54bc6c3ee70c15b192b0fd61
Matias <matiaspupirivarola@gmail.com>
2025-11-26 00:05:36 -0300

    cambio en el codigo

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 //const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser');
@@ -30,6 +31,32 @@ app.post('/ping', (req, res) => {
     res.json({ status: 'ok', body: req.body });
 });
 
+
+
+module.exports = app;
+*/
+const express = require('express');
+const pg = require('pg');
+const postsRoute = require('./routes/posts');
+const userRoute = require('./routes/user');
+
+const app = express();
+
+app.use(express.json({ limit: '10mb' }));
+
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+app.use("/posts", postsRoute)
+app.use("/user", userRoute);
+
+app.get('/', (req, res) => {
+    res.send('API funcionando en Vercel 🚀');
+});
+
+
+app.post('/ping', (req, res) => {
+    res.json({ status: 'ok', body: req.body });
+});
 
 
 module.exports = app;
