4832ae81dea1f5add8689a9e318c1ec421d94bc4
Matias <matiaspupirivarola@gmail.com>
2025-11-26 00:09:14 -0300

    otros cambios

--- app.js
+++ app.js
@@ -42,9 +42,9 @@ const userRoute = require('./routes/user');
 
 const app = express();
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
 
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use("/posts", postsRoute)
 app.use("/user", userRoute);
