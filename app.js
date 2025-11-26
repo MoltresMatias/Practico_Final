af53c084b395f91d425ef064c6e5ae1b6dbd9e18
Matias <matiaspupirivarola@gmail.com>
2025-11-26 00:17:37 -0300

    agregue cors

--- app.js
+++ app.js
@@ -42,6 +42,8 @@ const userRoute = require('./routes/user');
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json({ limit: '50mb' }));
 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
