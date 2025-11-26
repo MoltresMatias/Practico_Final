66bb8cf3d1b40d63e614423a16eeafe1cb87950e
Matias <matiaspupirivarola@gmail.com>
2025-11-26 01:14:57 -0300

    corrigiendo

--- server.js
+++ server.js
@@ -1,28 +1,8 @@
-/*
 const http = require('http');
-const app = require('./app');
-const port = 3000;
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('API funcionando en Vercel 🚀');
-});
-
-app.get('/ping', (req, res) => {
-    res.json({ status: 'ok', timestamp: new Date() });
-});
-
+const app = require('./app');   // importás el app ya configurado
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-server.listen(port);
-
-module.exports = app;
-*/
-
-
-const serverless = require('serverless-http');
-const app = require('./app');
-
-
-module.exports = serverless(app);
+server.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`);
+});
