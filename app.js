1a172953d90591cf90193e7fabed0e67829de920
Matias <matiaspupirivarola@gmail.com>
2025-11-25 23:54:54 -0300

    modifique algunas lineas

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const PORT = process.env.PORT || 3000
+//const PORT = process.env.PORT || 3000
+const bodyParser = require('body-parser');
+
 const pg = require('pg');
+
+// Middleware para parsear JSON y formularios
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 const app = express();
@@ -14,6 +20,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/posts", postsRoute)
 app.use("/user", userRoute);
+
+app.get('/', (req, res) => {
+    res.send('API funcionando en Vercel 🚀');
+});
+
 
 app.post('/ping', (req, res) => {
     res.json({ status: 'ok', body: req.body });
